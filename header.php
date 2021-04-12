594f40390cb83553e5f880096088d7b2243e0da9
faizannoman15 <82373854+faizannoman15@users.noreply.github.com>
2021-04-12 14:49:17 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>	</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+   </head>
+   <body>
+      <header>
+         <div class="container-fluid">
+         <div class="container mt-3">
+            <div class="row">
+               <div class="col-md-1">
+                  <img src="image/logo.png" class="lgo" style="z-index: auto; height: 66px;
+                     left: 125px;
+                     top: 15px;">
+               </div>
+               <div class="col-md-11">
+                  <ul class="nav justify-content-end nav-pills px-4" class="logo-sec">
+                     <li class="nav-item"><a href="#" class="nav-link  text-light">HOME</a></li>
+                     <li class="nav-item"><a href="#" class="nav-link  text-light">ABOUT US</a></li>
+                     <li class="nav-item"><a href="#" class="nav-link  text-light">THE TEAM</a></li>
+                     <li class="nav-item"><a href="#" class="nav-link  text-light">FAQ</a></li>
+                     <li class="nav-item"><a href="#" class="nav-link text-light">BLOG</a></li>
+                     <li class="nav-item"><a href="#" class="nav-link text-light">CONTACT US</a></li>
+                     <li class="nav-item"><button class="btn btn-warning rounded-pill text-light w-30 h-80">JOIN US</button></li>
+                  </ul>
+               </div>
+               <!----row---->
+            </div>
+            <!----container---->
+         </div>
+         <!----container fluid---->
+      </header>
+
+      <div class="jumbotron">
+         <div class="container-fluid mt-3">
+            <div class="text-light container" style="padding-top: 68px;">
+               <h3 style="font-size: 36px; margin-bottom: -6px; font-family: ui-serif; color: #c1c1c1">TAKE YOUR</h3>
+               <h2 style="font-size: 66px; font-family: ui-serif;">INVEST STRATEGY</h2>
+              <h3 style=" font-size: 34px; margin-bottom: -6px;font-family: ui-serif; color: #757575">TO THE NEXT LEVEL</h3><br>
+               <p  style=" font-size: 25px; font-family: ui-serif; color: #757575">A Profitable platform for high-margin <br>SPORTS investment.</p>
+            </div>
+            <img src="image/banner.jpg" class="img-fluid" style="width: 100%; height: 100%;">
+         </div>
+         <div class="container">
+         <button class="btn btn-warning rounded-pill text-light w-30 h-80">GET STARTED WITH</button>
+         </div>
+      </div>
+
+   </body>
+</html>
